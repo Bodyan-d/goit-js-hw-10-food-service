ac675d356023c2834572a0f6fb4e4f360f474b22
Bogdan Dyachenko <46127718+Bodyan-d@users.noreply.github.com>
2021-05-13 16:25:40 +0300

    add-toggle-on-themes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,22 @@ import cardsTpl from './templates/food-cards.hbs'
 import cardsSettings from './menu.json'
 
 const menuRef = document.querySelector('.js-menu');
+const themeSwitchToggleRef = document.querySelector('.theme-switch__toggle')
 const cardsMurkupRefs = cardsTpl(cardsSettings);
 
+themeSwitchToggleRef.addEventListener('click', themeToggle)
+
+function themeToggle(a) {
+    if (themeSwitchToggleRef.checked) {
+        document.body.classList.remove('light-theme');
+        document.body.classList.add('dark-theme');
+        return;
+    };
+
+    document.body.classList.remove('dark-theme');
+    document.body.classList.add('light-theme');
+
+}
+themeSwitchToggleRef.checked = false
 
 menuRef.insertAdjacentHTML('beforeend', cardsMurkupRefs);
