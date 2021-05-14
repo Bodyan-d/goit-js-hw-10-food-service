977d7234a0126d91db91a692d751538a06b573ab
Bogdan Dyachenko <46127718+Bodyan-d@users.noreply.github.com>
2021-05-14 21:19:59 +0300

    something

--- src/index.js
+++ src/index.js
@@ -4,20 +4,44 @@ import cardsSettings from './menu.json'
 const menuRef = document.querySelector('.js-menu');
 const themeSwitchToggleRef = document.querySelector('.theme-switch__toggle')
 const cardsMurkupRefs = cardsTpl(cardsSettings);
+menuRef.insertAdjacentHTML('beforeend', cardsMurkupRefs);
 
-themeSwitchToggleRef.addEventListener('click', themeToggle)
+themeSwitchToggleRef.addEventListener('input', themeToggle)
 
-function themeToggle(a) {
-    if (themeSwitchToggleRef.checked) {
-        document.body.classList.remove('light-theme');
-        document.body.classList.add('dark-theme');
+function themeToggle(e) {
+    if (localStorage.getItem('theme')) {
+        console.log(e.currentTarget.checked);
+        if (themeSwitchToggleRef.checked) {
+            document.body.classList.remove('light-theme');
+            document.body.classList.add('dark-theme');
+            localStoreSetitem(e);
+            return;
+        };
+
+        document.body.classList.remove('dark-theme');
+        document.body.classList.add('light-theme');
+        localStoreSetitem(e);
         return;
     };
 
-    document.body.classList.remove('dark-theme');
-    document.body.classList.add('light-theme');
+    localStoreGetitem(e);
 
 }
-themeSwitchToggleRef.checked = false
 
-menuRef.insertAdjacentHTML('beforeend', cardsMurkupRefs);
+function localStoreSetitem(e) {
+    if (!e.currentTarget.checked) {
+        localStorage.removeItem('theme')
+        localStorage.setItem('theme', 'light-theme')
+        console.log(localStorage.getItem('theme'));
+        return;
+    };
+    localStorage.removeItem('theme')
+    localStorage.setItem('theme', 'dark-theme')
+    console.log(localStorage.getItem('theme'));
+
+}
+
+function localStoreGetitem(e) {
+    document.body.classList.add(localStorage.getItem('theme'));
+
+}
